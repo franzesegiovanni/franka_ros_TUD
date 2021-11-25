a5895e9be0294141e257711e4b6c53e7c569b048
Giovanni Franzese <36767288+giovannifranzese94@users.noreply.github.com>
2021-11-25 15:37:38 +0100

    commented position/orientation_d_target

--- franka_example_controllers/src/cartesian_impedance_example_controller.cpp
+++ franka_example_controllers/src/cartesian_impedance_example_controller.cpp
@@ -105,8 +105,8 @@ bool CartesianImpedanceExampleController::init(hardware_interface::RobotHW* robo
 
   position_d_.setZero();
   orientation_d_.coeffs() << 0.0, 0.0, 0.0, 1.0;
-  position_d_target_.setZero();
-  orientation_d_target_.coeffs() << 0.0, 0.0, 0.0, 1.0;
+  //position_d_target_.setZero();
+  //orientation_d_target_.coeffs() << 0.0, 0.0, 0.0, 1.0;
   cartesian_stiffness_.setZero();
   cartesian_damping_.setZero();
 
